916ba997a9e4714ada27f95da7b89c3fc5191b0f
Davilus <odavialmeida@gmail.com>
2024-03-05 18:23:34 -0300

    Tudo certo agora, botei mesmo

---
+++ Plataforma 3D/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public bool attacking = false;
+    public GameObject hitBox;
+
+    public float attackingCounter;
+    public float attackingLength = 1f;
+
+
+
+    private void Update()
+    {
+        if (attackingCounter > 0)
+        {
+            attackingCounter -= Time.deltaTime;
+        }
+        else
+        {
+            hitBox.SetActive(false);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        attackingCounter = attackingLength;
+        hitBox.SetActive(true);
+        attacking = true;
+        Debug.Log("Attack");
+    }
+}
